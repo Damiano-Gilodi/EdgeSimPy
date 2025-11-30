969c4763f9a733b4c2f675b92e2741b09de18cb3
damiano.gilodi <damiano.gilodi@studenti.unimi.it>
2025-11-30 16:55:40 +0100

    RED: add test processing step  in service

--- tests/test_service.py
+++ tests/test_service.py
@@ -20,3 +20,28 @@ def test_start_processing():
     assert dp.processing_remaining_time == service.processing_time
     assert dp.size == service.processing_output
     assert service.processing_queue == [dp]
+
+
+def test_step_processing():
+
+    service = Service()
+    service.processing_time = 4
+    service.processing_output = 10
+
+    model = MagicMock()
+    service.model = model
+    service.model.schedule.steps = 3
+
+    dp = MagicMock(spec=DataPacket)
+    dp.is_processing = True
+    dp.processing_remaining_time = 1
+    dp.size = 0
+
+    service.processing_queue = [dp]
+
+    service.step()
+
+    assert dp.processing_remaining_time == 0
+    assert dp.is_processing is False
+    assert service.processing_queue == []
+    dp.launch_next_flow.assert_called_once_with(start_step=4)
