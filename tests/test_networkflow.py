9b8785030cb8511aca74620903f47a2ed9f36089
damiano.gilodi <damiano.gilodi@studenti.unimi.it>
2025-11-30 18:30:06 +0100

    RED: add test Networkflow.step type data_packet

---
+++ tests/test_networkflow.py
@@ -0,0 +1,28 @@
+from unittest.mock import MagicMock
+from edge_sim_py.components.data_packet import DataPacket
+from edge_sim_py.components.network_flow import NetworkFlow
+
+
+def test_step_flow_data_packet():
+
+    flow = NetworkFlow()
+    flow.status = "active"
+    flow.bandwidth = {0: 10, 1: 10, 2: 10}
+    flow.data_to_transfer = 0
+
+    dp = MagicMock(spec=DataPacket)
+    flow.metadata = {
+        "type": "data_packet",
+        "object": dp,
+        "index_hop": 0,
+        "index_link": 1,
+    }
+
+    model = MagicMock()
+    flow.model = model
+
+    flow.step()
+
+    dp.on_flow_finished.assert_called_once_with(flow)
+
+    flow.model.schedule.remove.assert_called_once_with(flow)
