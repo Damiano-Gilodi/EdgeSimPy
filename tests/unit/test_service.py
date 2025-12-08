148c52ab11b854f679fc6de616eeb16ec795c066
damiano.gilodi <damiano.gilodi@studenti.unimi.it>
2025-12-08 23:24:36 +0100

    RED: fix test_step_processing with correct launch_flow

--- tests/unit/test_service.py
+++ tests/unit/test_service.py
@@ -36,6 +36,11 @@ def test_step_processing():
     dp._is_processing = True
     dp._processing_remaining_time = 1
     dp.size = 0
+    dp._total_path = [[MagicMock(), MagicMock(), MagicMock(), MagicMock()], [MagicMock(), MagicMock(), MagicMock()]]
+    dp._current_hop = 0
+
+    dp._link_hops = [MagicMock()]
+    dp._link_hops[-1].end_time = 3
 
     service._processing_queue = [dp]
 
@@ -44,4 +49,9 @@ def test_step_processing():
     assert dp._processing_remaining_time == 0
     assert dp._is_processing is False
     assert service._processing_queue == []
-    dp._launch_next_flow.assert_called_once_with(start_step=4)
+    assert service._output_queue == [dp]
+
+    service.step()
+
+    dp._launch_next_flow.assert_called_once_with(start_step=7)
+    assert service._output_queue == []
